f73f68efc2d008727803527d5da4d3b80b0cca6e
miruts <miruts.hadush@aait.edu.et>
2020-01-31 15:47:57 +0300

    index html

--- delivery/http/handler/admin_home_handler.go
+++ delivery/http/handler/admin_home_handler.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params){
-	tmpl := template.Must(template.New("index").ParseFiles("index.html"))
+	tmpl := template.Must(template.New("index").ParseGlob("delivery/http/handler/*.html"))
 	tmpl.ExecuteTemplate(w, "index", nil)
 	return
 }
