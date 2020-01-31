8c9052423e75e4bca1bb8f34a8ecf7445d3e4e2c
miruts <miruts.hadush@aait.edu.et>
2020-01-31 09:26:11 +0300

    init commit

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
 go get github.com/jinzhu/gorm
 go get github.com/julienschmidt/httprouter
 go get github.com/jinzhu/gorm/dialects/postgres
-go get github.com/miruts/food-api/delivery/http/handler
-go get github.com/miruts/food-api/user/repository
-go get github.com/miruts/food-api/order/repository
-go get github.com/miruts/food-api/menu/repository
-go get github.com/miruts/food-api/user/service
-go get github.com/miruts/food-api/order/service
-go get github.com/miruts/food-api/menu/service
-go get github.com/miruts/food-api/entity
+go get github.com/miruts/food-api
 go build -o bin/application bin/application.go
