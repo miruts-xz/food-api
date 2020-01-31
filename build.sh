d45f81dea7ebd7b529a8531a0a4e63c89d192e16
miruts <miruts.hadush@aait.edu.et>
2020-01-31 09:40:34 +0300

    entity

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 go get github.com/jinzhu/gorm
 go get github.com/julienschmidt/httprouter
-go get github.com/jinzhu/gorm/dialects/postgres
 go get github.com/miruts/food-api
 go build -o bin/application bin/application.go
